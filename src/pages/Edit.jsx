e7a1b12c5ea187d97913ab159349331fd0c61100
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-11-25 23:23:43 +0530

    update logic

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,18 +1,155 @@
-import { collection, doc, getDoc, setDoc } from "firebase/firestore";
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { db } from "../firebaseConfig";
+import { Timestamp, addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { db, storage } from "../firebaseConfig";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 function Edit() {
-  const { _id } = useParams();
+  const { id } = useParams();
+  const [blogData,setBlogData] = useState({});
+  const navigate = useNavigate();
 
+
+  const [progress, setProgress] = useState(0);
+  
   useEffect(() => {
-    const blogsRef = doc(db, "Blogs", _id);
+    const blogsRef = doc(db, "Blogs", id);
     const blog = getDoc(blogsRef);
-    console.log(blog);
-  }, []);
 
-  return <div className="container"></div>;
+    blog.then((data) => {
+      var fetchedData = data.data();
+      setBlogData({
+        title: fetchedData.title,
+        author: fetchedData.author,
+        summary: fetchedData.summary,
+        imageUrl: fetchedData.imageUrl,
+        createdAt: fetchedData.createdAt 
+      })
+    })
+  }, []);
+  
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setBlogData({...blogData,
+      [e.target.name]: e.target.value
+    })
+  };
+  
+
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    setBlogData({ ...blogData, imageUrl: e.target.files[0] });
+  };
+
+  const handlePublish = (e) => {
+    e.preventDefault(); 
+
+    const storageRef = ref(
+      storage,
+      `/images/${Date.now()}${blogData.imageUrl.name}`
+    );
+
+    const uploadImage = uploadBytesResumable(storageRef, blogData.imageUrl);
+
+    uploadImage.on(
+      "state_changed",
+      (snapshot) => {
+        const progressPercent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progressPercent);
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        // setBlogData({
+        //   title: "",
+        //   author: "",
+        //   summary: "",
+        //   image: "",
+        //   descrption: "",
+        // });
+
+        getDownloadURL(uploadImage.snapshot.ref).then( async (urlImage) => {
+          console.log(urlImage)
+
+          await setDoc(doc(db, "Blogs", id), {
+            title: blogData.title,
+            author: blogData.author,
+            summary: blogData.summary,
+            imageUrl: urlImage,
+            createdAt: blogData.createdAt 
+          } )
+          // // addDoc(blogsRef, {
+          // //   title: formData.title,
+          // //   author: formData.author,
+          // //   summary: formData.summary,
+          // //   imageUrl: url,
+          // //   createdAt: Timestamp.now().toDate(),
+          // // })
+            // .then(() => {
+            //   console.log("Form data", blogData)
+            //   // alert("Blog Updated Successfully");
+            //   // setProgress(0);
+            //   // navigate("/");
+            // })
+            // .catch((err) => {
+            //   alert("Error: " + err);
+            // });
+        }).then(() => {
+          navigate("/")
+        })
+      }
+    );
+  };
+
+  return (
+    <div className="container">
+      <span className="page-title">Create your Blog Here</span>
+      <form action="" className="form">
+        <input
+          type="text"
+          name="title"
+          placeholder="Blog Title"
+          value={blogData.title}
+          onChange={handleOnChange}
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Auther Name"
+          value={blogData.author}
+          onChange={handleOnChange}
+        />
+        {/* <input
+          type="text"
+          name="summary"
+          placeholder="Summary"
+          value={blogData.summary}
+          onChange={handleOnChange}
+        /> */}
+        <input
+          type="file"
+          name="imageUrl"
+          id="imageUrl"
+          onChange={handleImageChange}
+        />
+        <textarea
+          name="summary"
+          id="summary"
+          cols="30"
+          rows="10"
+          placeholder="Description"
+          value={blogData.summary}
+          onChange={handleOnChange}
+        ></textarea>
+        <button onClick={handlePublish}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Edit;
